4729e75f51a0122ba86c68e9248e1864f6ca74f9
Ahmed Salem <as2720094@gmail.com>
2023-11-08 15:40:07 +0200

    First commit

---
+++ stacks/MyStack.ts
@@ -0,0 +1,15 @@
+import { StackContext, Api, EventBus } from "sst/constructs";
+
+export function API({ stack }: StackContext) {
+  const api = new Api(stack, "api", {
+    routes: {
+      "GET /": "packages/functions/src/lambda.handler",
+      "GET /todo": "packages/functions/src/todo.list",
+      "POST /todo": "packages/functions/src/todo.create",
+    },
+  });
+
+  stack.addOutputs({
+    ApiEndpoint: api.url,
+  });
+}
